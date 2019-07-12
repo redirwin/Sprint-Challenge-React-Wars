9166bc4fde3e8331d19fdd3c41b26b7b63e9cfa0
Dave Irwin <david@redirwin.com>
2019-07-12 16:12:30 -0700

    Refactored pagination and buttons.

--- starwars/src/components/GetChars.js
+++ starwars/src/components/GetChars.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import Loader from "react-loader-spinner";
 import styled from "styled-components";
 import DisplayChars from "./DisplayChars";
-import testData from "../testData";
 
 const UtilityContainer = styled.div`
   width: 100%;
@@ -13,7 +12,7 @@ const UtilityContainer = styled.div`
   flex-direction: column;
   justify-content: space-between;
 
-  .button-container {
+  .loader-container {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
@@ -31,49 +30,12 @@ const UtilityContainer = styled.div`
   }
 `;
 
-const Button = styled.div`
-  width: 10rem;
-  padding: 0.5rem 0;
-  background-color: #d6c178;
-  color: black;
-  font-weight: 600;
-  border: 1px solid black;
-  text-align: center;
-  cursor: pointer;
-  border-radius: 5px;
-  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.3),
-    0 6px 20px 0 rgba(255, 255, 255, 0.3);
-
-  &: active {
-    box-shadow: none;
-  }
-  @media screen and (max-width: 400px) {
-    margin-top: 0.5rem;
-  }
-`;
 
 export default function GetChars() {
   const [charList, updateCharList] = useState();
   const [pageStart, updatePageStart] = useState(1);
 
   console.log(pageStart);
-
-  const prevPageStart = usePrevious(pageStart);
-
-  function usePrevious(value) {
-    // This function researched and borrowed from https://usehooks.com/usePrevious
-    // The ref object is a generic container whose current property is mutable ...
-    // ... and can hold any value, similar to an instance property on a class
-    const ref = useRef();
-
-    // Store current value in ref
-    useEffect(() => {
-      ref.current = value;
-    }, [value]); // Only re-run if value changes
-
-    // Return previous value (happens before update in useEffect above)
-    return ref.current;
-  }
 
   useEffect(() => {
     axios
@@ -83,54 +45,31 @@ export default function GetChars() {
       .catch(err => console.log(err));
   }, [pageStart]);
 
-  return !charList || prevPageStart !== pageStart ? (
+  function getPage(e) {
+    e.target.value === "previous"
+      ? updatePageStart(pageStart - 1)
+      : updatePageStart(pageStart + 1);
+  }
+
+  console.log(charList);
+
+  return !charList ? (
     <UtilityContainer>
-      <div className="button-container">
+      <div className="loader-container">
         <div className="loader">
           <Loader type="ThreeDots" color="#00BFFF" height="100" width="100" />
         </div>
       </div>
     </UtilityContainer>
   ) : (
-    <UtilityContainer>
-      <div className="button-single">
-        {pageStart === 1 ? (
-          <div className="button-container">
-            <Button
-              type="submit"
-              onClick={() => {
-                updatePageStart(pageStart + 1);
-              }}
-            >
-              Next Page
-            </Button>
-          </div>
-        ) : (
-          <div className="button-container">
-            <Button
-              type="submit"
-              onClick={() => {
-                pageStart === 1
-                  ? updatePageStart(pageStart)
-                  : updatePageStart(pageStart - 1);
-              }}
-            >
-              Previous Page
-            </Button>
-            <Button
-              type="submit"
-              onClick={() => {
-                updatePageStart(pageStart + 1);
-              }}
-            >
-              Next Page
-            </Button>
-          </div>
-        )}
-      </div>
-      <div className="button-double">
-        {<DisplayChars charList={charList.results} />}
-      </div>
-    </UtilityContainer>
+    <div className="buttons-container">
+      <button className={charList.previous === null  ? "none" : "button" } onClick={getPage} value="previous">
+        Previous Page
+      </button>
+      <button className={charList.next === null  ? "none" : "button" } onClick={getPage} value="next">
+        Next Page
+      </button>
+      {<DisplayChars charList={charList.results} />}
+    </div>
   );
 }
